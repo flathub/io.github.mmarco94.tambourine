1679b62e0f982802431ce436247ddd1afbd3b9cc
Marco <802019+MMarco94@users.noreply.github.com>
2026-04-05 13:34:47 +0100

    Release 1.1.4
    This also changes the release process to generate SharedArchiveFile

---
+++ generate-classlist.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ex
+
+rm -rf classlist-generation
+mkdir classlist-generation
+
+# Downloading tambourine
+TAMBURINE_ARCHIVE=$(grep -oP 'https://github\.com/MMarco94/tambourine-music-player/releases/download/.*/tambourine\.tar\.gz' io.github.mmarco94.tambourine.yml)
+echo $TAMBURINE_ARCHIVE
+curl -L "$TAMBURINE_ARCHIVE" -o "classlist-generation/tambourine.tar.gz"
+tar -xzf "classlist-generation/tambourine.tar.gz" -C "classlist-generation"
+
+# Adding required JVM options
+echo "java-options=-Xshare:off" >> classlist-generation/tambourine/lib/app/tambourine.cfg
+echo "java-options=-XX:DumpLoadedClassList=tambourine.classlist" >> classlist-generation/tambourine/lib/app/tambourine.cfg
+classlist-generation/tambourine/bin/tambourine
+
+echo "Play a song, then close the window"
