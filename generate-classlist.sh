8d74a034d6d0a1585690414489447d8732e9f86f
Marco <802019+MMarco94@users.noreply.github.com>
2026-05-02 23:13:38 +0100

    Release 1.5.3

--- generate-classlist.sh
+++ generate-classlist.sh
@@ -6,7 +6,7 @@ rm -rf classlist-generation
 mkdir classlist-generation
 
 # Downloading tambourine
-TAMBURINE_ARCHIVE=$(grep -oP 'https://github\.com/MMarco94/tambourine-music-player/releases/download/.*/tambourine\.tar\.gz' io.github.mmarco94.tambourine.yml)
+TAMBURINE_ARCHIVE=$(grep -oP 'https://github\.com/MMarco94/tambourine-music-player/releases/download/.*/tambourine-amd64\.tar\.gz' io.github.mmarco94.tambourine.yml)
 echo $TAMBURINE_ARCHIVE
 curl -L "$TAMBURINE_ARCHIVE" -o "classlist-generation/tambourine.tar.gz"
 tar -xzf "classlist-generation/tambourine.tar.gz" -C "classlist-generation"
